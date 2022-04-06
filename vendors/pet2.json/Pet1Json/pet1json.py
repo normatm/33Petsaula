a862bb16f2b2cdb43753917cc50351cf562155bb
133Pets <norma_timoteo@yahoo.com.br>
2022-04-05 21:20:27 -0300

    Testes de API com a petstore.swagger.oi

---
+++ vendors/pet2.json/Pet1Json/pet1json.py
@@ -0,0 +1,17 @@
+{
+  "id": 179121,
+  "category": {
+    "id": 1,
+    "name": "dog"
+  },
+  "name": "Athena",
+  "photoUrls": [
+    "string"
+  ],
+  "tags": [
+    {
+      "id": 1,
+      "name": "Vacinado"
+    }
+  ],
+  "status": "available"
